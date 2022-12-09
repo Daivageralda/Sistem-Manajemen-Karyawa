972443b299d870cede3d0f53a836ee061270058f
Raihan Daiva Geralda <95012374+Daivageralda@users.noreply.github.com>
2022-12-09 11:19:06 +0800

    Add files via upload

---
+++ src/program/MainStart.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package program;
+
+/**
+ *
+ * @author HANZZ
+ */
+public class MainStart {
+    public static void main(String[] args) {
+        login lg = new login();
+        lg.setVisible(true);
+    }
+}
